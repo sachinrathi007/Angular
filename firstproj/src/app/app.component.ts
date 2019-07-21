4b0d9916d8420b922033ad322b28f87ac293fdab
Sachin Kumar Rathi <40701842+sachinrathi007@users.noreply.github.com>
2019-07-21 21:09:27 +0530

    First Angular Project
    First Angular Project

---
+++ firstproj/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'firstproj';
+  sno:number=100
+  fun1(){
+    alert("Hi")
+  }
+}
